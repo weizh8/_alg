e1d0fa236e87c3e9d60b649f69e6ac66d56916f7
weizh8 <vadergt001@gmail.com>
2026-01-05 23:45:18 +0800

    Create HillClimbing.py

---
+++ Final exam/HW 6/HillClimbing.py
@@ -0,0 +1,20 @@
+import random
+
+def f(x): return x**2
+
+def hill_climbing(start_x, step_size=0.1, iter=100):
+    current_x = start_x
+    for _ in range(iter):
+        
+        next_x_left = current_x - step_size
+        next_x_right = current_x + step_size
+        
+        
+        best_x = min([current_x, next_x_left, next_x_right], key=f)
+        
+        if best_x == current_x: # Jika tidak ada yang lebih baik, berhenti
+            break
+        current_x = best_x
+    return current_x
+
+print(f"Hill Climbing Result: {hill_climbing(10)}")
